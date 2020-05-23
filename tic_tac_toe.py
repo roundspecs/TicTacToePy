8bd9fa8e867802d08ff6c51035bf2611ca98dbd5
Zarif <2502.zarif@gmail.com>
2020-05-23 10:21:01 +0600

    created show_board

---
+++ tic_tac_toe.py
@@ -0,0 +1,10 @@
+board  = list(range(1,10))
+def show_board():
+    print("┌─┬─┬─┐")
+    for i in range(3):
+        print(f"│{board[i*3]}│{board[i*3+1]}│{board[i*3+2]}│")
+        if (i != 2):
+            print("├─┼─┼─┤")
+    print("└─┴─┴─┘")
+
+showBoard()
