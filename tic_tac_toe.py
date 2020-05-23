601cee395c0a1caa520a4415743c19384e719533
Zarif <2502.zarif@gmail.com>
2020-05-23 10:50:52 +0600

    created set_board changed the look of the board

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,17 +1,29 @@
 import os
 
 board  = list(range(1,10))
+user = '❌'
+
+for i in range(9):
+    board[i] = str(board[i]) + " "
+    
+
 def show_board():
-    print("┌─┬─┬─┐")
+    print("┌──┬──┬──┐")
     for i in range(3):
         print(f"│{board[i*3]}│{board[i*3+1]}│{board[i*3+2]}│")
         if (i != 2):
-            print("├─┼─┼─┤")
-    print("└─┴─┴─┘")
+            print("├──┼──┼──┤")
+    print("└──┴──┴──┘")
 
 def prompt():
     os.system('cls')
+    print('Turn: ' + user)
     show_board()
     return int(input('Position: '))
 
-prompt()
+def set_board(index):
+    board[index-1]=user
+
+while(True):
+    set_board(prompt())
+    user = '⭕' if user=='❌' else "❌"
