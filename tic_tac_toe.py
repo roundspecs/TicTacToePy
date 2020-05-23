39d69deb97e02f91ae6779740568e65605e8d2a2
Zarif <2502.zarif@gmail.com>
2020-05-23 11:35:11 +0600

    complete

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,6 +2,12 @@ import os
 
 board  = list(range(1,10))
 user = '❌'
+win_lines = [
+    [0, 1, 2], [0, 3, 6],
+    [3, 4, 5], [1, 4, 7],
+    [6, 7, 8], [2, 5, 8],
+    [3, 5, 7], [0, 4, 8]
+]
 
 for i in range(9):
     board[i] = str(board[i]) + " "
@@ -30,12 +36,23 @@ def set_board(index):
 def changeTurn(user):
     return '⭕' if user=='❌' else "❌"
 
+def did_win(user, board, win_lines):
+    for line in win_lines:
+        if board[line[0]]== user and board[line[1]]== user and board[line[2]]== user:
+            return True
+    return False
+
 turns = 0
 while(True):
     os.system('cls')
     print('Turn: ' + user)
     show_board()
-    if turns >= 9: break
+    if turns >= 9:
+        print("It's a draw. Both of you suck!")
+        break
     if(set_board(prompt()) == True):
+        if (did_win(user, board, win_lines)):
+            print(user + ' won!')
+            break
         user = changeTurn(user)
         turns += 1
